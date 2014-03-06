6f19912a7ca25d066c12be6505ec0c2743708b18
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-03-06 15:36:34 +0900

    POSTをSlackにそのまま流す

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,17 @@
 /*
  * GET home page.
  */
+var slackub = require('../lib/slackub')
+  , config  = require('../lib/config');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var context = slackub.init(config.url, config.token, config.channel, req.body);
+  context.post(function (code) {
+    console.log('10000: ' + code);
+    res.send(code);
+  }, function (code, e) {
+    res.send(code);
+    console.log('20000: ' + e.message);
+  });
 };
+
