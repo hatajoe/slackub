73fa664b03ef6c19e0250ff21933e1d7df338139
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-03-06 16:05:19 +0900

    GETパラメータでチャンネル指定

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var slackub = require('../lib/slackub')
   , config  = require('../lib/config');
 
 exports.index = function(req, res){
-  var context = slackub.init(config.url, config.token, config.channel, req.body);
+  var context = slackub.init(config.url, config.token, req.query.channel, req.body);
   context.post(function (code) {
     console.log('10000: ' + code);
     res.send(code);
