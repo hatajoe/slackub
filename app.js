6e77a5ae76398d71d84d623242f04ebef0c3ba7c
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-03-27 21:09:25 +0900

    LGTM!!

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,6 +26,7 @@ if ('development' == app.get('env')) {
 }
 
 app.post('/', routes.index);
+app.post('/lgtm', routes.lgtm);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var slackub = require('../lib/slackub')
+  , slacktm = require('../lib/slacktm')
   , config  = require('../lib/config');
 
 exports.index = function(req, res){
   var context = slackub.init(config.url, config.token, req);
+  context.post(function (code) {
+    res.send(code);
+  }, function (code, err) {
+    res.send(code);
+    console.log(err);
+  });
+};
+
+exports.lgtm = function(req, res){
+  var context = slacktm.init(config.url, config.token, req);
   context.post(function (code) {
     res.send(code);
   }, function (code, err) {
