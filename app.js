e849f3ba7407ee6797ee1ef90a841b1139525626
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-04-02 14:33:48 +0900

    create echo to slack

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ if ('development' == app.get('env')) {
 
 app.post('/', routes.index);
 app.post('/lgtm', routes.lgtm);
+app.post('/echo', routes.echo);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
