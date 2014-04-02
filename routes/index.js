96e336009da5bc205371568bb38f51da4fc23075
Yusuke Hatanaka <hatanaka@cloverlab.jp>
2014-04-02 14:33:48 +0900

    create echo to slack

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var slackub = require('../lib/slackub')
   , slacktm = require('../lib/slacktm')
+  , slackho = require('../lib/slackho')
   , config  = require('../lib/config');
 
 exports.index = function(req, res){
@@ -14,6 +15,16 @@ exports.index = function(req, res){
 
 exports.lgtm = function(req, res){
   var context = slacktm.init(config.url, config.token, req);
+  context.post(function (code) {
+    res.send(code);
+  }, function (code, err) {
+    res.send(code);
+    console.log(err);
+  });
+};
+
+exports.echo = function(req, res){
+  var context = slackho.init(config.url, config.token, req);
   context.post(function (code) {
     res.send(code);
   }, function (code, err) {
